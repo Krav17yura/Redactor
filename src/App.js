4cf62b8d22393ee5e54266f5b3959f5cf9a9296c
Yura Krav <krav17yura@mgail.com>
2020-12-04 13:04:52 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+import marked from 'marked'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [markdown, setMarkdown] = useState( '# sup ')
+
+    return (
+        <div className="app">
+            <textarea onChange={event => setMarkdown(event.target.value)} value={markdown}/>
+            <div
+                className="preview"
+                dangerouslySetInnerHTML={{__html: marked(markdown)}}
+            />
+        </div>
+    );
 }
 
 export default App;
